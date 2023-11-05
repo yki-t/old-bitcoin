8d9fbc2b97d4e45790f8a7dad7f1085b08bd0fd7
Yuki Tsujii (Desktop) <yuki.tsujii.37@gmail.com>
2023-11-05 10:51:46 +0900

    add download.sh

---
+++ download.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget http://download.oracle.com/berkeley-db/db-4.8.30.tar.gz
+wget https://src.fedoraproject.org/repo/pkgs/miniupnpc/miniupnpc-1.6.tar.gz/88055f2d4a061cfd4cfe25a9eae22f67/miniupnpc-1.6.tar.gz
+git clone https://github.com/bitcoin/bitcoin.git -b v0.5.1
